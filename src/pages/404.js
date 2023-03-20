ad8acf60a090ee493590871cf23c282b4e0de60d
KyleSkudlarek <kjs2wp@virginia.edu>
2023-03-19 19:15:45 -0500

    404.js styled component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,13 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import styled from 'styled-components';
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+const PageStyles = styled.main`
+  color: #232129;
+  padding: 96px;
+  fontFamily: -apple-system, Roboto, sans-serif, serif;
+`;
+
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
@@ -25,7 +27,7 @@ const codeStyles = {
 
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
+    <PageStyles>
       <h1 style={headingStyles}>Page not found</h1>
       <p style={paragraphStyles}>
         Sorry 😔, we couldn’t find what you were looking for.
@@ -40,7 +42,7 @@ const NotFoundPage = () => {
         <br />
         <Link to="/">Go home</Link>.
       </p>
-    </main>
+    </PageStyles>
   )
 }
 
